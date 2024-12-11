948ffa36ecbfd207362f5e5bf2b89752527df2f9
guusfelix2015 <gusfelix2015@gmail.com>
2024-12-10 23:06:38 -0300

    persistindo banco de dados

---
+++ src/database.js
@@ -0,0 +1,39 @@
+import fs from "node:fs/promises";
+
+const databasePatch = new URL("../db.json", import.meta.url);
+
+export class Database {
+  #database = {};
+
+  constructor() {
+    fs.readFile(databasePatch, "utf-8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasePatch, JSON.stringify(this.#database));
+  }
+
+  select(table) {
+    const data = this.#database[table] ?? [];
+
+    return data;
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database)) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+
+    this.#persist();
+
+    return data;
+  }
+}
